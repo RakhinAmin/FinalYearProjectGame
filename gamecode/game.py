b0f4ac94d4923851755ef99d44c582c73b8eef44
zjee015 <zjee015@live.rhul.ac.uk>
2023-11-17 18:22:19 +0000

    Updated game movement to use arrow keys to jump

--- gamecode/game.py
+++ gamecode/game.py
@@ -6,7 +6,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Simple Shooter Game')
+pygame.display.set_caption('Scrolling Shooter Game')
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -23,16 +23,32 @@ class Soldier(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.speed = speed
         self.direction_x = 0
+        self.direction_y = 0
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
+        self.jump = False
+        self.jump_height = 10
 
     def move(self, direction_x):
         self.direction_x = direction_x
+
+    def jump_start(self):
+        if not self.jump:
+            self.jump = True
+            self.jump_height = 10
 
     def update(self):
         self.rect.x += self.direction_x
         self.check_boundary_collision()
         self.check_enemy_collision()
+
+        if self.jump:
+            self.rect.y -= self.jump_height
+            self.jump_height -= 1
+
+            if self.rect.y >= 200:  # Modify the value as needed
+                self.rect.y = 200
+                self.jump = False
 
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
@@ -66,6 +82,8 @@ while run:
                 player.move(-player.speed)
             if event.key == pygame.K_RIGHT:
                 player.move(player.speed)
+            if event.key == pygame.K_UP:
+                player.jump_start()
 
     player.update()
     player.draw()
