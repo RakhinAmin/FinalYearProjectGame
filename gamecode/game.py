be4bc2538a7f1d61c1a3caaf15b18cc38291579f
zjee015 <zjee015@live.rhul.ac.uk>
2023-11-21 21:36:34 +0000

    Loading in the tile map to be displayed

--- gamecode/game.py
+++ gamecode/game.py
@@ -64,10 +64,11 @@ class Soldier:
             print("Player collision with enemy detected!")
 
 
-clock = pygame.time.Clock()
 pygame.init()
 
-pygame.display.set_caption('Scrolling schooter')
+clock = pygame.time.Clock()
+
+pygame.display.set_caption('Scrolling shooter')
 
 WINDOW_SIZE = (600, 400)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
@@ -79,6 +80,11 @@ grass_image = pygame.image.load('grass.png')
 TILE_SIZE = grass_image.get_width()
 
 dirt_image = pygame.image.load('dirt.png')
+
+background_objects = [[0.25, [120, 10, 70, 400]], [0.25, [280, 30, 40, 400]], [
+    0.5, [30, 40, 40, 400]], [0.5, [130, 90, 100, 400]], [0.5, [300, 80, 120, 400]]]
+
+true_scroll = [0, 0]
 
 game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
             ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
@@ -106,17 +112,50 @@ game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0
             ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']]
 
 while True:
-    screen.fill((144, 201, 120))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        player.handle_event(event)
+
+    true_scroll[0] += (player.rect.x - true_scroll[0] - 152) / 20
+    true_scroll[1] += (player.rect.y - true_scroll[1] - 106) / 20
+    scroll = true_scroll.copy()
+    scroll[0] = int(scroll[0])
+    scroll[1] = int(scroll[1])
+
+    pygame.draw.rect(screen, (7, 80, 75), pygame.Rect(0, 120, 300, 80))
+
+    for background_object in background_objects:
+        obj_rect = pygame.Rect(background_object[1][0] - scroll[0] * background_object[0],
+                               background_object[1][1] -
+                               scroll[1] * background_object[0],
+                               background_object[1][2], background_object[1][3])
+        if background_object[0] == 0.5:
+            pygame.draw.rect(screen, (14, 222, 150), obj_rect)
+        else:
+            pygame.draw.rect(screen, (9, 91, 85), obj_rect)
+
+    tile_rects = []
+    y = 0
+    for layer in game_map:
+        x = 0
+        for tile in layer:
+            if tile == '1':
+                screen.blit(
+                    dirt_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile == '2':
+                screen.blit(
+                    grass_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(
+                    x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
 
     player.update()
 
     player.draw(screen)
     enemy.draw(screen)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        player.handle_event(event)
 
     player.check_enemy_collision(enemy)
 
