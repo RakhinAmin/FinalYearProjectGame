79c499458f031005f1995b4552d562f5d54aa4e8
zjee015 <zjee015@live.rhul.ac.uk>
2023-11-24 21:32:07 +0000

    Set up player initial coordinates

--- proof-of-concept/playeranimation.py
+++ proof-of-concept/playeranimation.py
@@ -10,6 +10,21 @@ pygame.display.set_caption('Simple Shooter Game')
 
 
 class Player:
+        self.box_x = 100  # Initial x-coordinate of the snake's head
+        self.box_y = 100  # Initial y-coordinate of the snake's head
+        self.screen = pygame.display.set_mode(
+            (700, 700))  # Create the game window
+        pygame.display.set_caption("Snake Game")  # Set the window title
+
+        self.direction_x = 0  # Initial x-direction of the snake's movement
+        self.direction_y = 0  # Initial y-direction of the snake's movement
+        self.move_timer = 0  # Initialize a timer for controlling movement
+        # Set the interval for snake movement (milliseconds)
+        self.move_interval = 150
+        self.clock = pygame.time.Clock()  # Create a Pygame clock object
+
+        # Initialize with a starting segment
+        self.body = [pygame.Rect(self.box_x, self.box_y, 25, 25)]
 
     run = True
     while run:
