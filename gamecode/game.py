8549ca5e4b84dd66c895df83385907f379194c51
zjee015 <zjee015@live.rhul.ac.uk>
2023-11-24 19:13:56 +0000

    Fixed error where display did not refresh properly

--- gamecode/game.py
+++ gamecode/game.py
@@ -1,7 +1,4 @@
 import pygame
-from pygame.locals import *
-import noise
-import random
 
 
 class Soldier:
@@ -13,7 +10,7 @@ class Soldier:
         self.y_direction = 0
         self.air_time = 0
 
-    def update(self, tile_rects):
+    def update(self):
         self.player_movement = [0, 0]
         if self.move_right:
             self.player_movement[0] += 2
@@ -24,17 +21,15 @@ class Soldier:
         if self.y_direction > 3:
             self.y_direction = 3
 
-        self.rect, collisions = self.move(
-            self.rect, self.player_movement, tile_rects)
+        self.rect.x += self.player_movement[0]
+        self.rect.y += self.player_movement[1]
 
-        if collisions['bottom']:
-            self.air_time = 0
-            self.y_direction = 0
-        else:
-            self.air_time += 1
+        self.handle_collision()
 
-    def draw(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect)
+        self.air_time += 1
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
 
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
@@ -68,32 +63,6 @@ class Soldier:
         if self.rect.colliderect(enemy.rect):
             print("Player collision with enemy detected!")
 
-    def move(self, rect, movement, tiles):
-        collision_types = {'top': False, 'bottom': False,
-                           'right': False, 'left': False}
-        rect.x += movement[0]
-        hit_list = self.collision_test(rect, tiles)
-        for tile in hit_list:
-            if movement[0] > 0:
-                rect.right = tile.left
-                collision_types['right'] = True
-            elif movement[0] < 0:
-                rect.left = tile.right
-                collision_types['left'] = True
-        rect.y += movement[1]
-        hit_list = self.collision_test(rect, tiles)
-        for tile in hit_list:
-            if movement[1] > 0:
-                rect.bottom = tile.top
-                collision_types['bottom'] = True
-            elif movement[1] < 0:
-                rect.top = tile.bottom
-                collision_types['top'] = True
-        return rect, collision_types
-
-    def collision_test(self, rect, tiles):
-        return [tile for tile in tiles if rect.colliderect(tile)]
-
 
 pygame.init()
 
@@ -103,61 +72,52 @@ pygame.display.set_caption('Scrolling shooter')
 
 WINDOW_SIZE = (600, 400)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
-display = pygame.Surface((300, 200))
 
 player = Soldier(50, 50, 30, 30, (255, 0, 0))
 enemy = Soldier(400, 200, 30, 30, (0, 0, 255))
 
 grass_image = pygame.image.load('grass.png')
 TILE_SIZE = grass_image.get_width()
+
 dirt_image = pygame.image.load('dirt.png')
 
-tile_index = {
-    1: pygame.image.load('grass.png'),
-    2: pygame.image.load('dirt.png'),
-
-}
+background_objects = [[0.25, [120, 10, 70, 400]], [0.25, [280, 30, 40, 400]], [
+    0.5, [30, 40, 40, 400]], [0.5, [130, 90, 100, 400]], [0.5, [300, 80, 120, 400]]]
 
 true_scroll = [0, 0]
 
-moving_right = False
-moving_left = False
-vertical_momentum = 0
-air_timer = 0
+game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '2', '2', '0',
+                '2', '2', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['2', '2', '0', '0', '2', '2', '0', '0', '0', '0',
+                '0', '0', '0', '2', '2', '0', '0', '2', '2'],
+            ['1', '1', '2', '2', '1', '1', '2', '2', '2', '2',
+                '2', '2', '2', '1', '1', '2', '2', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']]
 
-CHUNK_SIZE = 8
+running = True
 
-
-def generate_chunk(x, y):
-    chunk_data = []
-    for y_pos in range(CHUNK_SIZE):
-        for x_pos in range(CHUNK_SIZE):
-            target_x = x * CHUNK_SIZE + x_pos
-            target_y = y * CHUNK_SIZE + y_pos
-            tile_type = 0  # nothing
-            height = int(noise.pnoise1(target_x * 0.1, repeat=9999999) * 5)
-            if target_y > 8 - height:
-                tile_type = 2  # dirt
-            elif target_y == 8 - height:
-                tile_type = 1  # grass
-            elif target_y == 8 - height - 1:
-                if random.randint(1, 5) == 1:
-                    tile_type = 3  # plant
-            if tile_type != 0:
-                chunk_data.append([[target_x, target_y], tile_type])
-    return chunk_data
-
-
-def load_map(path):
-    with open(path + '.txt', 'r') as f:
-        game_map = [list(row) for row in f.read().split('\n')]
-    return game_map
-
-
-game_map = load_map('map')
-
-while True:
-    display.fill((146, 244, 255))
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        player.handle_event(event)
 
     true_scroll[0] += (player.rect.x - true_scroll[0] - 152) / 20
     true_scroll[1] += (player.rect.y - true_scroll[1] - 106) / 20
@@ -165,65 +125,34 @@ while True:
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
 
+    screen.fill((144, 201, 120))
+
     tile_rects = []
-    for y in range(3):
-        for x in range(4):
-            target_x = x - 1 + int(round(scroll[0]/(CHUNK_SIZE*16)))
-            target_y = y - 1 + int(round(scroll[1]/(CHUNK_SIZE*16)))
-            target_chunk = str(target_x) + ';' + str(target_y)
-            if target_chunk not in game_map:
-                game_map[target_chunk] = generate_chunk(target_x, target_y)
-            for tile in game_map[target_chunk]:
-                display.blit(
-                    tile_index[tile[1]], (tile[0][0]*16-scroll[0], tile[0][1]*16-scroll[1]))
-                if tile[1] in [1, 2]:
-                    tile_rects.append(pygame.Rect(
-                        tile[0][0]*16, tile[0][1]*16, 16, 16))
+    y = 0
+    for layer in game_map:
+        x = 0
+        for tile in layer:
+            if tile == '1':
+                screen.blit(
+                    dirt_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile == '2':
+                screen.blit(
+                    grass_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(
+                    x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
 
-    player_movement = [0, 0]
-    if moving_right:
-        player_movement[0] += 2
-    if moving_left:
-        player_movement[0] -= 2
-    player_movement[1] += vertical_momentum
-    vertical_momentum += 0.2
-    if vertical_momentum > 3:
-        vertical_momentum = 3
+    player.update()
+    enemy.update()
 
-    player.rect, collisions = player.move(
-        player.rect, player_movement, tile_rects)
+    player.draw(screen)
+    enemy.draw(screen)
 
-    if collisions['bottom']:
-        air_timer = 0
-        vertical_momentum = 0
-    else:
-        air_timer += 1
+    player.check_enemy_collision(enemy)
 
-    pygame.draw.rect(display, (255, 0, 0), (player.rect.x -
-                     scroll[0], player.rect.y - scroll[1], player.rect.width, player.rect.height))
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-        if event.type == KEYDOWN:
-            if event.key == K_RIGHT:
-                moving_right = True
-            if event.key == K_LEFT:
-                moving_left = True
-            if event.key == K_UP:
-                if air_timer < 6:
-                    vertical_momentum = -5
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-        if event.type == KEYUP:
-            if event.key == K_RIGHT:
-                moving_right = False
-            if event.key == K_LEFT:
-                moving_left = False
-
-    screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
     pygame.display.update()
     clock.tick(60)
-
 
 pygame.quit()
