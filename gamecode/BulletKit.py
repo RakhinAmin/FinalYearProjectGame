9effe424d03280d6d6800d199fe77f415a9f4e1f
zjee015 <zjee015@live.rhul.ac.uk>
2024-03-17 16:37:00 +0000

    bullet game object setup and deleting old game files

---
+++ gamecode/BulletKit.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame import Vector2
+
+from Game.GameObject import GameObject
+from utils.assets_manager import assetsManager
+
+
+class BulletKit(GameObject):
+    def __init__(self, pos):
+        super().__init__(pos, assetsManager.get("bulletKit"))
+        self.onGround = False
+        self.prevPos = pos
+
+    def update(self):
+        self.prevPos = Vector2(self.pos.x, self.pos.y)
+        super().update()
+        if not self.onGround:
+            self.applyForce(Vector2(0, 0.52))
