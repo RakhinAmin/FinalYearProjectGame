d24250cd577fc5d1554fa0705c4ee319960f84c0
zjee015 <zjee015@live.rhul.ac.uk>
2024-03-27 20:52:40 +0000

    file to control the rendering time of the player bullet

---
+++ gamecode/PlayerBullet.py
@@ -0,0 +1,19 @@
+from pygame import Vector2
+
+from Game.GameObject import GameObject
+from utils.assets_manager import assetsManager
+from utils.util import utils
+
+
+class PlayerBullet(GameObject):
+    def __init__(self, pos, dirX):
+        super().__init__(pos, assetsManager.get("playerBullet"))
+        self.cDestroyTime = 0
+        self.vel = Vector2(dirX, 0) * 10
+
+    def update(self):
+        self.pos += self.vel
+
+        self.cDestroyTime += utils.deltaTime()
+        if self.cDestroyTime > 5:
+            self.destroyFlag = True
