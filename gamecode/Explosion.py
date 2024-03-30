a294c7db2a55230d206a8b8323d85107584cabee
zjee015 <zjee015@live.rhul.ac.uk>
2024-03-30 20:04:53 +0000

    enemybullet and explosion effect files

---
+++ gamecode/Explosion.py
@@ -0,0 +1,22 @@
+from Game.GameObject import GameObject
+from utils.SpriteSheet import SpriteSheet
+from utils.assets_manager import assetsManager
+
+
+class Explosion(GameObject):
+    def __init__(self, pos):
+        super().__init__(pos,None)
+
+        self.animSheet = SpriteSheet(assetsManager.get("explo1"), 1, 10)
+        self.animSheet.setPlay(0, 9, 0.07, False)
+        self.img = self.animSheet.getCurrentFrame()
+
+
+    def update(self):
+        if self.animSheet.current >= 9:
+            self.destroyFlag = True
+
+        super().update()
+        self.animSheet.play()
+        self.img = self.animSheet.getCurrentFrame()
+
