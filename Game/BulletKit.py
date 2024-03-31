a36de458192786a7b88671a182a58b7b5f4e7c7d
zjee015 <zjee015@live.rhul.ac.uk>
2024-04-01 00:08:22 +0100

    character images were invisible to reverted to slightly older build

--- Game/BulletKit.py
+++ Game/BulletKit.py
@@ -1,30 +1,18 @@
 import pygame
 from pygame import Vector2
 
-# Importing necessary modules and classes.
 from Game.GameObject import GameObject
 from utils.assets_manager import assetsManager
 
-# BulletKit class, a type of GameObject for the shootable bullet
-
 
 class BulletKit(GameObject):
-    # Constructor for the BulletKit class.
-    def __init__(self, pos):
-        # Call the constructor of the base class (GameObject) with the position and the image from the assets manager.
-        super().__init__(pos, assetsManager.get("bulletKit"))
-        # Initialize whether the BulletKit is on the ground.
+    def __init__(self,pos):
+        super().__init__(pos,assetsManager.get("bulletKit"))
         self.onGround = False
-        # Store the previous position, the same as the current position.
         self.prevPos = pos
 
-    # Update method called every frame.
     def update(self):
-        # Update the previous position to the current position at the start of each frame.
-        self.prevPos = Vector2(self.pos.x, self.pos.y)
-        # Call the update method of the base class to handle position and movement.
+        self.prevPos = Vector2(self.pos.x,self.pos.y)
         super().update()
-        # Apply gravity to the Bullet if it's not on the ground.
         if not self.onGround:
-            # Apply a downward force to simulate gravity (gravity drop on bullet with distance)
-            self.applyForce(Vector2(0, 0.52))
+            self.applyForce(Vector2(0,0.52))
--- Game/GameObject.py
+++ Game/GameObject.py
@@ -1,88 +1,57 @@
 import pygame.rect
 from pygame.math import Vector2
-from utils.util import utils  # importing the utils module
+from utils.util import utils
 
-# Importing the Enum class from the enum module.
 from enum import Enum
-
-# Definition of the GameObject class.
 
 
 class GameObject:
 
-    # Constructor for the GameObject class.
-    def __init__(self, pos, img, visible=True):
-        # Initial position of the object (Vector2 is a 2D vector class from Pygame).
+    def __init__(self, pos, img,visible = True):
         self.pos = pos
-        # Image/sprite representing the object.
         self.img = img
-        # Velocity vector (initially set to zero).
         self.vel = Vector2(0, 0)
-        # Acceleration vector (initially set to zero).
         self.acc = Vector2(0, 0)
-        # Visibility state of the object (visible by default).
         self.visible = visible
 
-        # Flag to indicate if the object should be destroyed.
         self.destroyFlag = False
-        # Flag to indicate if the image should be flipped horizontally.
         self.flipX = False
-        # Health of the object (-1 indicates possibly unlimited or undefined health).
         self.health = -1
-        # Damage the object can inflict (-1 indicates possibly no damage or undefined).
         self.damage = -1
-        # Placeholder for a health bar object (if any).
         self.healthBar = None
 
-    # Method to update the object's state.
     def update(self):
-        # Update velocity by adding acceleration.
-        self.vel += self.acc
-        # Update position by adding velocity.
-        self.pos += self.vel
-        # Reset acceleration after applying it.
+        self.vel = Vector2(self.vel.x + self.acc.x, self.vel.y + self.acc.y)
+        self.pos = Vector2(self.pos.x + self.vel.x, self.pos.y + self.vel.y)
         self.acc = Vector2(0, 0)
 
-    # Method to apply a force (acceleration) to the object.
     def applyForce(self, f):
-        # Add the force vector to the object's acceleration.
-        self.acc += f
+        self.acc = Vector2(self.acc.x + f.x, self.acc.y + f.y)
 
-    # Method to handle the object being hit by another object.
-    def hit(self, obj):
-        # Decrease health by the damage inflicted by the other object.
+    def hit(self,obj):
         self.health -= obj.damage
-        # Set destroy flag if health drops to 0 or below.
-        if self.health <= 0:
+        if self.health <= 0 :
             self.destroyFlag = True
 
-    # Method to draw the object on the screen.
     def draw(self):
-        # Don't draw if the object is not visible.
         if not self.visible:
             return
 
-        # Flip the image horizontally if needed.
         if self.flipX:
             self.img = pygame.transform.flip(self.img, True, False)
 
-        # Draw the image at the object's current position.
-        utils.screen.blit(self.img, (self.pos.x, self.pos.y))
+        adjusted_y = self.getRect().y - utils.scroll[1]
+        utils.display.blit(self.img, (self.pos.x - utils.scroll[0], adjusted_y ))
 
-    # Method to get the player objects rendering image.
     def getRect(self):
-        # Create and return a pygame.Rect object based on the object's position and image dimensions.
-        return pygame.rect.Rect(self.pos.x, self.pos.y, self.img.get_width(), self.img.get_height())
+        rect = pygame.rect.Rect(self.pos.x, self.pos.y, self.img.get_width(), self.img.get_height())
+        return rect
 
-    # Method to set the object's position.
     def setPos(self, pos):
         self.pos = pos
 
-    # Method to get the object's current position.
     def getPos(self):
         return self.pos
 
-    # Method to get the center position of the object.
     def getCenter(self):
-        # Calculate and return the center point based on the object's position and dimensions.
         return Vector2(self.pos.x + self.getRect().w / 2, self.pos.y + self.getRect().h / 2)
